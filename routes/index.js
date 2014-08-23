710562bb9fed46c4c0ad4cc448f328bcf13f7d9e
JhonnatanRodriguez <jhonn@outlook.com>
2014-08-23 19:40:56 -0400

    restructured

--- routes/index.js
+++ routes/index.js
@@ -14,31 +14,31 @@ module.exports = function(app){
 
 					app.post("/charge", function (req, res) {
 
-					  var name = req.body.cardholdername;
-					  var CardNum =  req.body.CardNum;
-					  var date =  req.body.months + req.body.year;
-					  var CVNum = req.body.CVNum;
-					  var Amount = req.body.Amount;
+						  var name = req.body.cardholdername;
+						  var CardNum =  req.body.CardNum;
+						  var date =  req.body.months + req.body.year;
+						  var CVNum = req.body.CVNum;
+						  var Amount = req.body.Amount;
 
-							  var Data = {
-							  		name: name,
-							  		CardNum: CardNum,
-							  		ExpDate: date,
-							  		CVNum: CVNum,
-							  		Amount: Amount
-							  };
+								  var Data = {
+								  		name: name,
+								  		CardNum: CardNum,
+								  		ExpDate: date,
+								  		CVNum: CVNum,
+								  		Amount: Amount
+								  };
 
-							  Risepay.auth(Data, function(Databack){
+								  Risepay.auth(Data, function(Databack){
 
-							  });
+								  });
 
-									var message = "Thanks " + name + " for you Purchase.";
-									  var msg = {
-									  	title: "Your Order is Procesing!",
-									  	message: message
-									  };
+										var message = "Thanks " + name + " for you Purchase.";
+										  var msg = {
+										  	title: "Your Order is Procesing!",
+										  	message: message
+										  };
 
-					  					res.send(msg, 200);
+						  					res.send(msg, 200);
 
 					});
 
