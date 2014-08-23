84d4b0ed1cbe429225716bddee63b4824a2c7247
JhonnatanRodriguez <jhonn@outlook.com>
2014-08-23 18:54:22 -0400

    Reestructuración y Libreria actualizada
    - Renombre la carpeta modele por lib (donde se encuentra la librería)
    -Actualize el modulo RisePay (reestructura)

---
+++ routes/index.js
@@ -0,0 +1,47 @@
+
+var Payments = require("../lib/test");
+
+module.exports = function(app){
+
+
+
+				app.get("/", function(req, res){
+						res.render("index", {});
+					});
+
+
+					app.post("/charge", function (req, res) {
+
+					  var name = req.body.cardholdername;
+					  var CardNum =  req.body.CardNum;
+					  var date =  req.body.months + req.body.year;
+					  var CVNum = req.body.CVNum;
+					  var Amount = req.body.Amount;
+
+						  var Data = {
+						  		name: name,
+						  		CardNum: CardNum,
+						  		ExpDate: date,
+						  		CVNum: CVNum,
+						  		Amount: Amount
+						  };
+
+						  Payments.createTransaction(Data, function(Databack){
+
+						  });
+
+						var message = "Thanks " + name + " for you Purchase.";
+						  var msg = {
+						  	title: "Your Order is Procesing!",
+						  	message: message
+						  };
+
+					  		res.send(msg, 200);
+
+					});
+
+
+
+
+
+}
