e83bff0868ce7d24e7d653a247ffebbc3b79af6b
JhonnatanRodriguez <jhonn@outlook.com>
2014-08-28 05:00:33 -0400

    Functions new + manage Exceptions + XML to JSON

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,20 @@
 	var risePay = require('../lib/risepay');
 
 	risePay.connect({
-		username: "YourUsername",
-		password: "YourPassword" 
+		username: "username",
+		password: "password" 
 	});
+
+var parser = require('xml2json');
+
 
 module.exports = function(app){
 
 //Testing Api via Console
 
 							app.get("/", function(req, res){
-									//res.render("index", {});
 
-									 risePay.sale({
+									 risePay.auth({
 								  	NameOnCard: "Jhonny",
 									CardNum : "5149612222222229",
 									ExpDate : "1214",
@@ -24,15 +26,16 @@ module.exports = function(app){
 									Zip: "33139",
 									Street: "1880 Alton Road",
 									}, function(data){
+											
 
-						  					res.send(data);
+						  					res.send(data.RespMSG);
 									});	
 
 								});
 
 
-					app.post("/charge", function (req, res) {
-
+					app.get("/test", function (req, res) {
+						
 					});
 
 
