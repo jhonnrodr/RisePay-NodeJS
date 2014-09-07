b85f8651d4a63b154e76baa9d586d41de7d6b5e7
JhonnatanRodriguez <jhonn@outlook.com>
2014-09-07 14:27:37 -0400

    fixd

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 
-	var risePay = require('risepay');
+	var risePay = require('../lib/risepay');
 
 	risePay.connect({
-		username: "demo",
-		password: "demo" 
+		username: "jhonndev",
+		password: "U0H464z4" 
 	});
 
 
@@ -20,15 +20,17 @@ module.exports = function(app){
 									InvNum: "ABC123",
 									Zip: "33139",
 									Street: "1880 Alton Road",
+									TipAmt: 1,
 									Customer: "JR"
+
 									}, function(resp){
 											if(resp.Approved){
-												console.log("Approved. Transaction ID = " + resp.PNRef);
-												console.log("AuthCode = " + resp.AuthCode)
+												console.log("Approved. Transaction ID = " + resp.json.PNRef);
+												console.log("AuthCode = " + resp.json.AuthCode)
 											}else{
-												console.log("Declined: " + resp.Message);
+												console.log("Declined: " + resp.json.Message);
 											}
-						  					res.send(resp);
+						  					res.send(resp.json);
 									});	
 
 								});
